64e636ff26cfa7ec4458b6b6e46062d903db9adc
JavierNavas <navasmilton7@gmail.com>
2016-07-28 21:45:40 -0600

    inicio de reconocimiento de lenguaje

--- MIA_201314014/main.c
+++ MIA_201314014/main.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef struct {
+  int tamano;
+}BloqueInfo;
+
 int opcion=0;
 int tamano=0;
 char comando[256];
+char analizo[2][256];
+int contador=0;
+char nombrearchivo[20];
+
 int main()
 {
     printf("Bienvenido a FILE SYSTEM EXT2/EXT3\n");
-    printf("Ingrese un comando:\n");
-    gets(comando);
+    printf("Ingrese un comando :\n");
+    fgets(comando, 256, stdin);
     tamano = strlen(comando);
-     switch(opcion)
-     {
-     case 1:
-      printf("---------------\n\n");
-      break;
-     case 2:
-      printf("---------------\n\n");
-      break;
-     default:
-       printf("comando erroneo\n");
-       printf("---------------\n\n");
-     }
+
+    char * pch;
+    pch = strtok (comando," ");
+    while (pch != NULL)
+    {
+    strcpy(analizo[contador], pch);
+    printf ("esta:%s\n",pch);
+    pch = strtok (NULL," ");
+    contador++;
+    }
+
+    if( strcmp(analizo[0],"exec") == 0 ) //aqui comparo si ambos nombres son iguales
+        printf("son iguales");
+    else
+        printf("Comando no reconocido");
+
+
     return 0;
 }
+
